8d36a892a4003000b71d7289ecf39013af60d966
9xhacker <113031029+9xhacker@users.noreply.github.com>
2022-09-17 01:18:53 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+#code_recorded_by_9x-hacker
+
+clear
+
+sleep 1
+
+echo ""
+
+echo ""
+
+echo " 
+
+████████╗███████╗░█████╗░██╗░░██╗
+
+╚══██╔══╝██╔════╝██╔══██╗██║░░██║
+
+░░░██║░░░█████╗░░██║░░╚═╝███████║
+
+░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+
+░░░██║░░░███████╗╚█████╔╝██║░░██║
+
+░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝ " | lolcat
+
+echo ""
+
+sleep 1
+
+clear
+
+pkg install php -y
+
+pkg install openssh -y
+
+pkg install unzip -y
+
+pkg install curl -y
+
+sleep 1
+
+clear
+
+echo "Loading==> " | lolcat
+
+sleep 1
+
+clear
+
+echo "Loading====> " | lolcat
+
+sleep 1
+
+clear
+
+echo "Loading=======> " | lolcat
+
+sleep 1
+
+clear
+
+echo " done " | lolcat
+
+sleep 1
+
+clear
+
+unzip techcochi.zip
+
+clear
+
+rm -rf techcochi.zip
+
+clear
+
+bash about.sh
+
+sleep 4
+
+echo ""
+
+echo ""
+
+echo " SUBSCRIBE MY CHANNEL 🔔 " | lolcat
+
+xdg-open https://youtube.com/channel/UC9QTZEZsu-7kyxzaGq4D8Mg
+
+echo ""
+
+sleep 10
+
+echo " set your browser "
